47cf7df1b7276f54fc0a31571ff4aea8849ebd9f
董家宏 <dongjiahong@hotmail.com>
2016-09-13 18:35:33 +0800

    daily modify

--- cpp/http/my_http.cc
+++ cpp/http/my_http.cc
@@ -13,7 +13,7 @@
 #define HTTP_GET "GET /%s HTTP/1.1\r\nHOST: %s:%d\r\nAccept: */*\r\n\r\n"  
   
   
-static int http_tcpclient_create(const char *host, int port){  
+static int http_tcp_client_create(const char *host, const int &port){  
     struct hostent *he;  
     struct sockaddr_in server_addr;   
     int socket_fd;  
@@ -37,65 +37,65 @@ static int http_tcpclient_create(const char *host, int port){
     return socket_fd;  
 }  
   
-static void http_tcpclient_close(int socket){  
+static void http_tcp_client_close(int socket){  
     close(socket);  
 }  
   
-static int http_parse_url(const char *url,char *host,char *file,int *port)  
+static int http_parse_url(const char *url, char *host, char *file, int &port)  
 {  
-    char *ptr1,*ptr2;  
+    char *phost, *pfile;  
     int len = 0;  
-    if(!url || !host || !file || !port){  
+    if(url == NULL || host == NULL || file == NULL || port != 0){  
         return -1;  
     }  
   
-    ptr1 = (char *)url;  
+    phost = (char *)url;  
   
-    if(!strncmp(ptr1,"http://",strlen("http://"))){  
-        ptr1 += strlen("http://");  
+    if(!strncmp(phost, "http://", strlen("http://"))){  
+        phost += strlen("http://");  
     }else{  
         return -1;  
     }  
   
-    ptr2 = strchr(ptr1,'/');  
-    if(ptr2){  
-        len = strlen(ptr1) - strlen(ptr2);  
-        memcpy(host,ptr1,len);  
+    pfile = strchr(phost, '/');  
+    if(pfile != NULL){  
+        len = strlen(phost) - strlen(pfile);  
+        memcpy(host, phost, len);  
         host[len] = '\0';  
-        if(*(ptr2 + 1)){  
-            memcpy(file,ptr2 + 1,strlen(ptr2) - 1 );  
-            file[strlen(ptr2) - 1] = '\0';  
+        if(*(pfile + 1)){  
+            memcpy(file, pfile + 1, strlen(pfile) - 1);  
+            file[strlen(pfile) - 1] = '\0';  
         }  
     }else{  
-        memcpy(host,ptr1,strlen(ptr1));  
-        host[strlen(ptr1)] = '\0';  
+        memcpy(host,phost,strlen(phost));  
+        host[strlen(phost)] = '\0';  
     }  
     //get host and ip  
-    ptr1 = strchr(host,':');  
-    if(ptr1){  
-        *ptr1++ = '\0';  
-        *port = atoi(ptr1);  
+    phost = strchr(host,':');  
+    if(phost != NULL){  
+        *phost++ = '\0';  
+        port = atoi(phost);  
     }else{  
-        *port = MY_HTTP_DEFAULT_PORT;  
+        port = MY_HTTP_DEFAULT_PORT;  
     }  
   
     return 0;  
 }  
   
   
-static int http_tcpclient_recv(int socket,char *lpbuff){  
-    int recvnum = 0;  
+static int http_tcp_client_recv(int socket, char *lpbuff){  
+    int recv_num = 0;  
   
-    recvnum = recv(socket, lpbuff,BUFFER_SIZE*4,0);  
+    recv_num = recv(socket, lpbuff,BUFFER_SIZE * 4, 0);  
   
-    return recvnum;  
+    return recv_num;  
 }  
   
-static int http_tcpclient_send(int socket,char *buff,int size){  
-    int sent=0,tmpres=0;  
+static int http_tcp_client_send(int socket, char *buff, const int &size){  
+    int sent=0, tmpres=0;  
   
     while(sent < size){  
-        tmpres = send(socket,buff+sent,size-sent,0);  
+        tmpres = send(socket, buff+sent, size-sent, 0);  
         if(tmpres == -1){  
             return -1;  
         }  
@@ -104,31 +104,31 @@ static int http_tcpclient_send(int socket,char *buff,int size){
     return sent;  
 }  
   
-static char *http_parse_result(const char*lpbuf)  
+static char *http_parse_result(const char* lpbuf)  
 {  
     char *ptmp = NULL;   
     char *response = NULL;  
     ptmp = (char*)strstr(lpbuf,"HTTP/1.1");  
-    if(!ptmp){  
+    if(ptmp == NULL){  
         printf("http/1.1 not faind\n");  
         return NULL;  
     }  
-    if(atoi(ptmp + 9)!=200){  
+    if(atoi(ptmp + 9) != 200){  
         printf("result:\n%s\n",lpbuf);  
         return NULL;  
     }  
   
-    ptmp = (char*)strstr(lpbuf,"\r\n\r\n");  
-    if(!ptmp){  
+    ptmp = (char*)strstr(lpbuf, "\r\n\r\n");  
+    if(ptmp == NULL){  
         printf("ptmp is NULL\n");  
         return NULL;  
     }  
     response = (char *)malloc(strlen(ptmp)+1);  
-    if(!response){  
+    if(response == NULL){  
         printf("malloc failed \n");  
         return NULL;  
     }  
-    strcpy(response,ptmp+4);  
+    strcpy(response, ptmp+4);  
     return response;  
 }  
   
@@ -144,38 +144,38 @@ char * http_post(const char *url,const char *post_str){
     int len=0;  
     char *response = NULL;  
   
-    if(!url || !post_str){  
-        printf("      failed!\n");  
+    if(url == NULL || post_str == NULL){  
+        printf("failed!\n");  
         return NULL;  
     }  
   
-    if(http_parse_url(url,host_addr,file,&port)){  
+    if(http_parse_url(url, host_addr, file, port)){  
         printf("http_parse_url failed!\n");  
         return NULL;  
     }  
     //printf("host_addr : %s\tfile:%s\t,%d\n",host_addr,file,port);  
   
-    socket_fd = http_tcpclient_create(host_addr,port);  
+    socket_fd = http_tcp_client_create(host_addr, port);  
     if(socket_fd < 0){  
-        printf("http_tcpclient_create failed\n");  
+        printf("http_tcp_client_create failed\n");  
         return NULL;  
     }  
        
-    sprintf(lpbuf,HTTP_POST,file,host_addr,port,strlen(post_str),post_str);  
+    sprintf(lpbuf, HTTP_POST, file, host_addr, port, strlen(post_str), post_str);  
   
-    if(http_tcpclient_send(socket_fd,lpbuf,strlen(lpbuf)) < 0){  
-        printf("http_tcpclient_send failed..\n");  
+    if(http_tcp_client_send(socket_fd, lpbuf, strlen(lpbuf)) < 0){  
+        printf("http_tcp_client_send failed..\n");  
         return NULL;  
     }  
     //printf("发送请求:\n%s\n",lpbuf);  
   
     /*it's time to recv from server*/  
-    if(http_tcpclient_recv(socket_fd,lpbuf) <= 0){  
-        printf("http_tcpclient_recv failed\n");  
+    if(http_tcp_client_recv(socket_fd, lpbuf) <= 0){  
+        printf("http_tcp_client_recv failed\n");  
         return NULL;  
     }  
   
-    http_tcpclient_close(socket_fd);  
+    http_tcp_client_close(socket_fd);  
   
     return http_parse_result(lpbuf);  
 }  
@@ -192,36 +192,36 @@ char * http_get(const char *url)
     int port = 0;  
     int len=0;  
   
-    if(!url){  
-        printf("      failed!\n");  
+    if(url == NULL){  
+        printf("failed!\n");  
         return NULL;  
     }  
   
-    if(http_parse_url(url, host_addr, file, &port)){  
+    if(http_parse_url(url, host_addr, file, port)){  
         printf("http_parse_url failed!\n");  
         return NULL;  
     }  
     //printf("host_addr : %s\tfile:%s\t,%d\n",host_addr,file,port);  
   
-    socket_fd =  http_tcpclient_create(host_addr,port);  
+    socket_fd =  http_tcp_client_create(host_addr, port);  
     if(socket_fd < 0){  
-        printf("http_tcpclient_create failed\n");  
+        printf("http_tcp_client_create failed\n");  
         return NULL;  
     }  
   
-    sprintf(lpbuf,HTTP_GET,file,host_addr,port);  
+    sprintf(lpbuf, HTTP_GET, file, host_addr, port);  
   
-    if(http_tcpclient_send(socket_fd,lpbuf,strlen(lpbuf)) < 0){  
-        printf("http_tcpclient_send failed..\n");  
+    if(http_tcp_client_send(socket_fd, lpbuf, strlen(lpbuf)) < 0){  
+        printf("http_tcp_client_send failed..\n");  
         return NULL;  
     }  
 //  printf("发送请求:\n%s\n",lpbuf);  
   
-    if(http_tcpclient_recv(socket_fd,lpbuf) <= 0){  
-        printf("http_tcpclient_recv failed\n");  
+    if(http_tcp_client_recv(socket_fd,lpbuf) <= 0){  
+        printf("http_tcp_client_recv failed\n");  
         return NULL;  
     }  
-    http_tcpclient_close(socket_fd);  
+    http_tcp_client_close(socket_fd);  
   
     return http_parse_result(lpbuf);  
 } 
